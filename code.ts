59f1a53206d46bc8fec624b9e03f1c4f4fc756d5
steveoh-rose <steven@oh-rose.com>
2023-07-22 14:48:21 +1000

    Initial commit for Design Tokens Generator

---
+++ code.ts
@@ -0,0 +1,15 @@
+// This plugin will open a tab that indicates that it will monitor the current
+// selection on the page. It cannot change the document itself.
+
+// This file holds the main code for plugins. Code in this file has access to
+// the *figma document* via the figma global object.
+// You can access browser APIs in the <script> tag inside "ui.html" which has a
+// full browser environment (See https://www.figma.com/plugin-docs/how-plugins-run).
+
+// This shows the HTML page in "ui.html".
+figma.showUI(__html__);
+
+// This monitors the selection changes and posts the selection to the UI
+figma.on('selectionchange', () => {
+  figma.ui.postMessage(figma.currentPage.selection)
+});
